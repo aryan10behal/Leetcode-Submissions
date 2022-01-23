cd42be6414818679c81a6bc49335d01c5eb6a039
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-01-23 15:35:33 +0530

    Time: 884 ms (30.77%), Space: 392.8 MB (7.69%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -7,20 +7,25 @@ public:
         {
             int good = 0;
             queue<int> q;
-            int z = i;
+            int bitmask = i;
             int j = 0;
             vector<int> gd(n, 0);
-            while(z!=0)
+            while(bitmask!=0)
             {
-                if(z&1 == 1)
+                if(bitmask&1 == 1)
                 {
                     q.push(j);
                     gd[j] = 1;
                 }
-                z>>=1;
+                bitmask>>=1;
                 j++;
             }
+            
+            // count the number of 1s in our bitmask or people who we assumed to be good...
             good = q.size();
+            
+            
+            // bfs on assumed 'good' people to check if their statements contradict!!
             while(q.size())
             {
                 int k = q.front();
@@ -29,6 +34,10 @@ public:
                 int contradiction = 0;
                 for(int x: statements[k])
                 {
+                    // checking for contradiction....
+                    // if a good person says someone is good but he is not assumed to good
+                    // or
+                    // if he is called bad but we assumed him to be good...
                     if((x == 1 && gd[j]!=1) || (x == 0 && gd[j]==1))
                     {
                         contradiction = 1;
@@ -36,6 +45,7 @@ public:
                     }   
                     j++;
                 }
+                // if contradiction, we will avoid this result... so assigning -INF to good
                 if(contradiction)
                 {
                     good = -1e9;
