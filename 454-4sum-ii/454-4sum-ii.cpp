cab185f1764f764b40ed2a67b9cf23354321178e
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-02-03 14:53:46 +0530

    Time: 602 ms (7.88%), Space: 28.5 MB (6.41%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        map<int, int> a,b;
+        int ans = 0;
+        for(int x: nums1)
+        {
+            for(int y: nums2)
+                a[x+y]++;
+        }
+        for(int x: nums3)
+        {
+            for(int y: nums4)
+                b[x+y]++;
+        }
+        for(auto w: a)
+        {
+           ans += b[-1*w.first]*w.second;
+        }
+        return ans;
+    }
+};
