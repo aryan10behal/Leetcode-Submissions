179ab7aa6f63a708adab84eaf6402fcd1805b7a5
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-02-07 18:46:12 +0530

    Time: 49 ms (46.52%), Space: 31.9 MB (86.25%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& c, vector<int>& g, int min) {
+        int n = c.size();
+        
+        int m = -1, s = 0, z = 0;
+        int i = 0, j = 0, st = -1, en = -1;
+        while(i<min-1)
+        {
+            if(g[i] == 1)
+                z++;
+            s+=c[i];
+            i++;
+        }
+        while(i<n)
+        {
+            if(g[i] == 1)
+            { 
+                z++;
+                s+=c[i];
+            }
+            if(z>0)
+            {
+                if(s > m)
+                    st = j, en = i, m = s;
+            }
+            if(g[j] == 1)
+            {
+                z--;
+                s-=c[j];
+            }
+            i++, j++;
+        }
+        int ans = 0;
+        cout<<st<<" "<<en<<endl;
+        for(int i = 0;i<n;i++)
+        {
+            if(g[i] == 0)
+                ans+=c[i];
+            else
+            {
+                if(i>=st && i<=en)
+                    ans+=c[i];
+            }
+        }
+        return ans;
+    }
+};
