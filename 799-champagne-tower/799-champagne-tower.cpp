f51ae4a6462d0b161a730051a68efe1de0035643
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-03-04 21:12:59 +0530

    Time: 3 ms (95.95%), Space: 6 MB (75.19%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double a[102][102];
+        memset(a, 0, sizeof(a));
+        a[0][0] = (double)poured;
+        for(int r = 0; r<= query_row; r++)
+        {
+            for(int c = 0; c<=r; c++)
+            {
+                double q = (a[r][c] - 1.0) / 2.0;
+                if(q>0)
+                {
+                    a[r+1][c] += q;
+                    a[r+1][c+1] += q;
+                }
+            }
+        }
+        return min(1.0, a[query_row][query_glass]);
+    }
+};
