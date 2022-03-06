4f1c140345d0e1dd71a504b285c83d9aca4adda4
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-03-07 04:43:52 +0530

    Time: 12 ms (95.73%), Space: 10.7 MB (41.60%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -4,13 +4,13 @@ public:
         vector<vector<int>> ans;
         for(int i = 0;i<(1<<n);i++)
         {
+            if(__builtin_popcount(i) != k)
+                continue;
             vector<int> x;
             for(int j = 0;j<n;j++)
             {
                 if((i>>j)&1)
                     x.push_back(j+1);
-                if(x.size()>k)
-                    break;
             }
             if(x.size() == k)
                 ans.push_back(x);
