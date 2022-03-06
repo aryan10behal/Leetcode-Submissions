d2a59ecd285fd0a5239f0eacb2db0669e74a88b9
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-03-07 03:55:02 +0530

    Time: 20 ms (89.13%), Space: 16.8 MB (74.94%) - LeetHub

--- 87-scramble-string/87-scramble-string.cpp
+++ 87-scramble-string/87-scramble-string.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool help(string s1, string s2, int l, int h, map<pair<string, string>, bool> &m)
+    bool help(string s1, string s2, map<pair<string, string>, bool> &m)
     {
         if(s1 == s2)
             return true;
-        if(l > h)
-            return false;
-        if(l==h)
-            return s1[l] == s2[l];
+        int n = s1.size();
+        if(n == 1)
+            return s1[0] == s2[0];
         if(m.find({s1,s2}) != m.end())
             return m[{s1,s2}];
-        if(h-l == 1)
+        if(n == 2)
         {
-            bool a = ((s1[l] == s2[l] and s1[h] == s2[h]) or (s1[h] == s2[l] and s1[l] == s2[h]));
+            bool a = ((s1[0] == s2[0] and s1[1] == s2[1]) or (s1[1] == s2[0] and s1[0] == s2[1]));
             m[{s1, s2}] = a;
             return a;
         }
@@ -24,7 +23,7 @@ public:
             return false;
         vector<int> a(26, 0), b(26, 0);
         vector<int> p;
-        for(int i = l;i<=h;i++)
+        for(int i = 0;i<n;i++)
         {
             a[s1[i]-'a']++;
             b[s2[i]-'a']++;
@@ -37,18 +36,18 @@ public:
                     break;
                 }
             }
-            if(f == 1 && i!=h)
+            if(f == 1 && i!=n-1)
                 p.push_back(i);
         }
         for(int i = 0;i<p.size();i++)
         {
-            string aa = s1.substr(l, p[i]+1);
-            string bb = s2.substr(l, p[i]+1);
+            string aa = s1.substr(0, p[i]+1);
+            string bb = s2.substr(0, p[i]+1);
             string cc = s1.substr(p[i]+1);
             string dd = s2.substr(p[i]+1);
             
-            bool a = help(aa, bb, 0, aa.size()-1, m);
-            bool b = help(cc, dd, 0, cc.size()-1, m);
+            bool a = help(aa, bb, m);
+            bool b = help(cc, dd, m);
             if(a && b)
             {
                 m[{s1,s2}] = true;
@@ -60,7 +59,7 @@ public:
         
         vector<int> a1(26, 0), b1(26, 0);
         vector<int> p2;
-        for(int i = l, j = h;i<=h;i++, j--)
+        for(int i = 0, j = s1.size()-1;i<n;i++, j--)
         {
             a1[s1[i]-'a']++;
             b1[s2[j]-'a']++;
@@ -73,21 +72,19 @@ public:
                     break;
                 }
             }
-            if(f == 1 && i!=h)
+            if(f == 1 && i!=n-1)
             {
-                //cout<<i<<endl;
                 p2.push_back(i);
             }
         }
         for(int i = 0;i<p2.size();i++)
         {
-            string xx = s2.substr(h-p2[i], p2[i]+1);
-            string jj = s2.substr(l, h-l-p2[i]);
-            string yy = s1.substr(l, p2[i]+1);
-            string zz = s1.substr(p2[i]+1, h-l-p2[i]);
-            //cout<<yy<<" "<<xx<<" "<<zz<<" "<<jj<<endl;
-            bool a = help(yy, xx, 0, yy.size() -1, m);
-            bool b = help(zz, jj, 0, zz.size()-1, m);
+            string xx = s2.substr(n-1-p2[i], p2[i]+1);
+            string jj = s2.substr(0, n-1-p2[i]);
+            string yy = s1.substr(0, p2[i]+1);
+            string zz = s1.substr(p2[i]+1, n-1-p2[i]);
+            bool a = help(yy, xx, m);
+            bool b = help(zz, jj,  m);
             if(a && b)
             {
                 m[{s1,s2}] = true;
@@ -99,6 +96,6 @@ public:
     }
     bool isScramble(string s1, string s2) {
         map<pair<string, string>, bool> m;
-        return help(s1, s2, 0, s1.size()-1, m);
+        return help(s1, s2, m);
     }
 };
