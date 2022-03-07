3d687023574c0863f682dea13edda483d7b0e362
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-03-08 00:41:26 +0530

    Time: 1008 ms (5.00%), Space: 5.9 MB (81.29%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void permute(string &v, int &k, int l, int h, string &ans)
+    {
+        if(k<0)
+            return;
+        if(l == h-1)
+        {
+            k--;
+            if(k == 0)
+            {
+                ans = v;
+            }
+            return;
+        }
+        for(int i = l;i<h;i++)
+        {
+            {
+                char s = v[i];
+                v[i] = v[l];
+                v[l] = s;
+            }
+            sort(v.begin()+l+1, v.end());
+            permute(v, k, l+1, h, ans);
+            sort(v.begin()+l+1, v.end());
+            {
+                char s = v[i];
+                v[i] = v[l];
+                v[l] = s;
+            }
+        }
+    }
+    string getPermutation(int n, int k) {
+        string v = "";
+        string ans = "";
+        for(int i=1;i<=n;i++)
+        {
+            v += (i+'0');
+        }
+        permute(v, k, 0, n, ans);
+        return ans;
+    }
+};
