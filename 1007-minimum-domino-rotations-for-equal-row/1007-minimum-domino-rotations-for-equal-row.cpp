1a0852f1cd26481b25b3429f8b16b88e4c586ec4
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-03-20 13:35:04 +0530

    Time: 250 ms (19.27%), Space: 111.6 MB (29.27%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int n = tops.size();
+        vector<int> arr(6, 0), arr2(6, 0), arr3(6, 0);
+        for(int i=0;i<n;i++)
+        {
+            arr[tops[i]-1]++;
+            arr2[bottoms[i]-1]++;
+            if(tops[i] == bottoms[i])
+                arr3[tops[i]-1]++;
+        }
+        int ans = 1e9;
+        for(int i = 0;i<6;i++)
+        {
+            if(arr[i]+arr2[i] - arr3[i] >= n)
+            {
+                cout<<n-arr[i]<<" "<<n-arr2[i]<<endl;
+                ans = min(ans, min(n-arr[i], n-arr2[i]));
+            }
+        }
+        return ans == 1e9? -1: ans;
+    }
+};
