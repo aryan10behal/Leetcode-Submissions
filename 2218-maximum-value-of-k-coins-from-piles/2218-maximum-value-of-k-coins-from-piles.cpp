4019a0b0747ece8d904041d915e8fd5b4ae966e9
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-03-27 20:33:32 +0530

    Time: 386 ms (50.00%), Space: 28.5 MB (16.67%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[2001][2001] = {0};
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        return solve(0,piles,k);
+    }
+    int solve(int index, vector<vector<int>>& piles, int k){
+        if(index>=piles.size()) return 0;
+        if(dp[index][k] != 0) return dp[index][k];
+        int maxi=0,sum=0;
+		//either dont take from that pile
+        maxi = max(maxi,solve(index+1,piles,k));
+        for(int i=0;i<piles[index].size();i++){
+            sum += piles[index][i];
+			//or take some certain prefix from that pile, if we can take that much elements.
+            if(k-(i+1)>=0) maxi = max(maxi,sum+solve(index+1,piles,k-(i+1)));
+        }
+        return dp[index][k] = maxi;
+    }
+};
