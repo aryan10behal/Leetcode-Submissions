0cea68ed1829a5b1d03a5c2505945364177abf8c
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-05-30 12:44:49 +0530

    Time: 18 ms (8.09%), Space: 6 MB (30.45%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,8 +1,38 @@
 class Solution {
 public:
+    int count(unsigned int x)
+    {
+        int a = 0;
+        while(x!=0)
+            x>>=1, a++;
+        return a;
+    }
     int divide(int dividend, int divisor) {
         if(dividend == -2147483648 and divisor == -1)
             return 2147483647;
-        return dividend/divisor;
+        if(divisor == 1)
+            return dividend;
+        if(divisor == -1)
+            return -dividend;
+        int sign = 1;
+        if(dividend > 0 and divisor < 0 or dividend < 0 and divisor > 0)
+            sign = -1;
+        int val = 0;
+        //cout<<m<<" "<<n<<endl;
+        unsigned int dividing = abs(dividend);
+        unsigned int divider = abs(divisor);
+        int m = count(dividing), n = count(divider);
+        cout<<m<<" "<<n<<endl;
+        for(int i = m-n;i>=0;i--)
+        {
+            //cout<<(dividend>>i)<<endl;
+            if((dividing>>i) >= divider)
+            {
+                dividing -= (divider<<i);
+                val += (1<<i);
+                cout<<val<<"$"<<endl;
+            }
+        }
+        return sign*val;
     }
 };
