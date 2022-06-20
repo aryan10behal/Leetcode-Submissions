37739e1a5f6825bda9fbf605da90a47be43baffe
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-06-20 22:44:30 +0530

    Time: 1449 ms (5.45%), Space: 13.9 MB (93.56%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        int n = words.size();
+        vector<int> dis_set(n, -1);
+        for(int i = 0; i<n;i++)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                if(dis_set[i] == -1 or dis_set[j] == -1)
+                {
+                    int x = words[i].size()-1, y = words[j].size()-1;
+                    while(x>=0 and y>=0)
+                    {
+                        if(words[i][x] != words[j][y])
+                            break;
+                        x--, y--;
+                    }
+                    if(x == -1 or y == -1)
+                    {
+                        //cout<<words[i]<<" "<<words[j]<<endl;
+                        if(x == -1)
+                            dis_set[i] = j;
+                        else
+                            dis_set[j] = i;
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i =0 ;i<n;i++)
+        {
+            if(dis_set[i] == -1)
+            {
+                //cout<<words[i]<<" ";
+                ans+=words[i].size()+1;
+            }
+        }
+        return ans;
+    }
+};
