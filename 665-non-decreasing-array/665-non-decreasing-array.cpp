25d4defd6e8eccca7c5d3131fa1ab382d8431ce2
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-06-25 11:51:59 +0530

    Time: 75 ms (5.14%), Space: 27.8 MB (7.47%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -16,16 +16,9 @@ public:
         for(int i = 0;i<n-1;i++)
         {
             if(nums[i] > nums[i+1])
-               { 
                  p|=1;
-               }
-        }
-        for(int i = 0;i<n-1;i++)
-        {
-            if(xx[i] > xx[i+1])
-               { 
+             if(xx[i] > xx[i+1])
                  q|=1;
-               }
         }
         return (p&q==1)?false:true;
     }
