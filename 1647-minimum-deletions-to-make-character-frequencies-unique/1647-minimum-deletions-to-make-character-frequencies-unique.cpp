c78f1ebba20ca3bfc8e070096aaca3c3f82af5dd
aryan10behal <55681065+aryan10behal@users.noreply.github.com>
2022-06-28 11:39:27 +0530

    Time: 49 ms (96.16%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char, int> mp;
+        vector<int> v(26, 0);
         for(char x: s)
-            mp[x]++;
-        vector<int> v;
-        for(auto x: mp)
-        {
-            // cout<<x.first<<" "<<x.second<<endl;
-            v.push_back(x.second);
-        }
+            v[x-'a']++;
         sort(v.begin(), v.end(), greater<int>());
         int mx = v[0]-1, ans = 0;
         for(int i = 1;i<v.size();i++)
         {
+            if(v[i]==0)
+                break;
             if(mx < v[i])
                 ans += v[i]-mx;
-            // cout<<v[i]<<" ";
             mx = max(0,min(v[i], mx)-1);
-            // cout<<mx<<" "<<ans<<endl;
         }
         return ans;
     }
