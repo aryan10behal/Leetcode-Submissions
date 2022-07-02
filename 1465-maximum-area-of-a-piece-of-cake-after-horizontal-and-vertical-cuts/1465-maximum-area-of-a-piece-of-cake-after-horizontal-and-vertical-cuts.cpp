b669c4a7040ef46a2c21060100ed5a8fcf4cfaa1
Aryan Behal <55681065+aryan10behal@users.noreply.github.com>
2022-07-02 19:16:14 +0530

    Time: 98 ms (58.35%), Space: 32.3 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -8,11 +8,9 @@ public:
             xx = max(xx, hc[i] - hc[i-1]);
         for(int i=1;i<vc.size();i++)
             yy = max(yy, vc[i] - vc[i-1]);
-        //cout<<xx<<" "<<yy<<endl;
         xx = max(xx, max(hc[0], h - hc[hc.size()-1]));
         yy = max(yy, max(vc[0], w - vc[vc.size()-1]));
         long long a = xx, b = yy;
-       // cout<<a<<" "<<b<<endl;
         int m = 1e9+7;
         return (a*b)%m;
     }
