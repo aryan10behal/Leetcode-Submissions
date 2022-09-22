def22686eb7a440e91e36dfcf101179be22178d6
Aryan Behal <55681065+aryan10behal@users.noreply.github.com>
2022-09-22 15:15:40 +0530

    Time: 63 ms (58.44%), Space: 14.7 MB (46.10%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,7 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.split(' ')
-        for i in range(len(s)):
-            s[i] = s[i][::-1]
-        s = ' '.join(s)
-        return s
+        return ' '.join([x[::-1] for x in s.split(' ')])
