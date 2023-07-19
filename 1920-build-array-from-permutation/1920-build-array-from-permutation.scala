5403833b35b4c2b9a1c8390abeb7a71972036b35
Aryan Behal <55681065+aryan10behal@users.noreply.github.com>
2023-07-19 11:31:21 +0530

    Time: 619 ms (55.56%), Space: 56.1 MB (44.44%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.scala
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.scala
@@ -1,11 +1,5 @@
-import scala.collection.mutable.ArrayBuffer
 object Solution {
     def buildArray(nums: Array[Int]): Array[Int] = {
-        val ans = ArrayBuffer[Int]();
-        for(i <- 0 to nums.length-1)
-        {
-            ans += nums(nums(i));
-        }
-        return ans.toArray;
+       nums.map(x=>nums(x)); 
     }
 }
